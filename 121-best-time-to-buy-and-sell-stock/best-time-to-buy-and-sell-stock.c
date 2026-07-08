0cfdb69583d7a5345cd1599fa014a31388df32bc
kan1shk-07 <Kanishk.arise143@gmail.com>
2026-07-08 21:15:08 +0530

    Time: 4 ms (16.53%) | Memory: 16.1 MB (14.63%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,22 @@
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize <= 1) return 0;
+    
+    int max_profit = 0;
+    int min_price = prices[0];
+    
+    for (int i = 1; i < pricesSize; i++) {
+
+        if (prices[i] < min_price) {
+            min_price = prices[i];
+        } 
+        
+        else {
+            int current_profit = prices[i] - min_price;
+            if (current_profit > max_profit) {
+                max_profit = current_profit;
+            }
+        }
+    }
+    
+    return max_profit;
+}
